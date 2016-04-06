5578a543948c9606cad896e4262ee02944119486
Tina Truong <tina.truong21@gmail.com>
2016-04-06 17:45:21 -0400

    added feature to edit pictures

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+patch 'pictures/:id' => "pictures#update"
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
