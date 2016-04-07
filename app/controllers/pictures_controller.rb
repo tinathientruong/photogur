9b2e800f6186ce164b6fe9c79365a92d7ccabd0b
Tina Truong <tina.truong21@gmail.com>
2016-04-07 10:02:39 -0400

    refactored

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -25,18 +25,28 @@ class PicturesController < ApplicationController
   end
 
   def edit
-   @picture = Picture.find(params[:id])
+    @picture = Picture.find(params[:id])
   end
 
- def update
-   @picture = Picture.find(params[:id])
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
 
-   if @picture.update_attributes(picture_params)
-     redirect_to "/pictures/#{@picture.id}"
-   else
-     render :edit
-   end
-end
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+
+
+
+
 
   private
   def picture_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,10 @@
-
-
 Rails.application.routes.draw do
+
   root 'pictures#index'
 
-  get 'pictures' => 'pictures#index'
+  resources :pictures
 
-  post 'pictures' => 'pictures#create' # this is a new line of code
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id' => 'pictures#show', as: 'picture'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-patch 'pictures/:id' => "pictures#update"
-end
+ end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
